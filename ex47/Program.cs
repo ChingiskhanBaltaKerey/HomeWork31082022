127c09344179dae3afedc03dfd5f975824bc2fa3
Chingiskhan Nugrazin <110384143+ChingiskhanBaltaKerey@users.noreply.github.com>
2022-08-31 13:18:24 +0600

    Домашнее задание 7 семинар

---
+++ ex47/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int [,] CteateArray (int rows, int cols)
+{
+    return new int [rows, cols];
+}
+void Fill(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols =  array.GetLength (1);
+    for (int row = 0; row < rows; row++)
+{ 
+    for (int col = 0; col < cols; col++)
+    {
+        array [row, col] = new Random ().Next(-10,10);
+    }
+    }
+
+}
+void Print (int [,]array)
+{
+    int rows = array.GetLength (0);
+    int cols = array.GetLength (1);
+    for (int row = 0; row < rows; row++)
+{ 
+    for (int col = 0; col < cols; col++)
+    {
+       Console.Write($"{array [row,col],3}");
+    }
+    Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 5;
+
+int [,] matrix = CteateArray (m,n);
+Fill (matrix);
+Print (matrix); 
+
---
+++ ex52/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+
+
+int [,] array = new int [3,4];
+int srAr = 0;
+
+void Fill (int[,] array)
+{
+    int[,] array = new int[3, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+    for (int i = 0; i < 3; i++)
+    {
+        int srAr = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            srAr += array[i, j];
+            
+        }
+        Console.Write($"Cреднее арифметическое элементов столбца {i + 1} = {(float)srAr / 3}");
+        }
+}
+void Print (int [,]array)
+{
+    int[,] array = new int[3, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+    for (int i = 0; i < 3; i++)
+    {
+        int srAr = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            srAr += array[i, j];
+            
+        }
+        Console.Write($"Cреднее арифметическое элементов столбца {j + 1} = {(float)srAr / 3}");
+        }
+}
+Fill (srAR);
+Print (srAr);
